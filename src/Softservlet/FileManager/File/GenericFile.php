584eb28e2a9e6720e6fb537ade7ff3f5d14bd4b4
Marius <marius@softservlet.com>
2014-03-25 18:44:50 +0000

    generic file constructor params

--- src/Softservlet/FileManager/File/GenericFile.php
+++ src/Softservlet/FileManager/File/GenericFile.php
@@ -1,12 +1,22 @@
 <?php namespace Softservlet\FileManager\File;
 
+use Softservlet\FileManager\Deliver\UriParser;
+
 class GenericFile implements FileInterface
 {
 	protected $uri;
 	
-	public function __construct($uri)
+	public function __construct($uri, $schema = null)
 	{
 		$this->uri = $uri;
+		
+		if(!stristr($this->uri,'://')) {
+			if($schema == null) {
+				$this->uri = 'file://' . $this->uri;
+			} else {
+				$this->uri = $schema . '://' . $this->uri;
+			}
+		}
 	}
 	
 	public function uri()
